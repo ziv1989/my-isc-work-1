5ce8c550f562a4989bad0c3b5968df2d6e491c37
Matt Amos <m.amos1@lancaster.ac.uk>
2017-11-16 06:00:01 +0000

    Reading serial ports and plotting datetime

---
+++ python_work/python_probes.py
@@ -0,0 +1,38 @@
+#!/bin/env python2.7
+import serial
+import datetime as dt
+import matplotlib
+import matplotlib.pyplot as plt
+
+ser = serial.Serial(
+    port = '/dev/ttyUSB0',
+    baudrate = 9600,
+    bytesize = serial.EIGHTBITS,
+    parity = serial.PARITY_NONE,
+    stopbits = serial.STOPBITS_ONE)
+
+data = float(ser.read(size=8).strip()[1:6])
+print dt.datetime.utcnow().isoformat(),str(data)
+
+j=0
+temp_temp=[]
+time_list=[]
+
+while j<20:
+    data = float(ser.read(size=8).strip()[1:6])
+    time = dt.datetime.utcnow()
+    print time ,str(data)
+    temp_temp.append(data)
+    time_list.append(time)
+    j+=1     
+
+new_times = matplotlib.dates.date2num(time_list)
+
+
+plt.plot_date(new_times,temp_temp,'r--')
+plt.xlabel('Time')
+plt.ylabel('Temperature in degrees')
+plt.gcf().autofmt_xdate()
+plt.show()
+
+
